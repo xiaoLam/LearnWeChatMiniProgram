2ba907f12ad75029a78105e8682abadd46108e1e
xiaoLam <1484611740@qq.com>
2020-11-29 14:20:19 +0800

    小程序初体验

--- pages/home/home.js
+++ pages/home/home.js
@@ -1,66 +1,34 @@
 // pages/home/home.js
 Page({
-
-  /**
-   * 页面的初始数据
-   */
+  // 像vue一样在data中定义好变量
   data: {
-
+    name : "xiaoLam",
+    age : 18,
+    students:[
+      {name: "xiaohong", age: 11},
+      {name: "xiaoming", age: 12},
+      {name: "xiaojun", age: 10},
+    ],
+    counter: 0
   },
+  // 直接在Page中传入的对象中编写对应的点击事件
+  increBtn() {
+    // 1.错误的做法
+    /* this.data.counter + 1
+    console.log(this.data.counter); */
+    // 如果是这种做法的话, 会发现data中的counter确实有增加, 但是页面中的counter并没有增加
+    // 原因是因为小程序中并没有像vue一样通过definProperty来监听data数据的改变的
+    // 如果想要页面也跟着更改的话, 那么就需要使用 this.setDate()来操作
 
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-
+    // 2.正确的做法
+    this.setData({
+      counter: this.data.counter + 1,
+    })
+    console.log(this.data.counter);
   },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+  decreBtn() {
+    this.setData({
+     counter : this.data.counter - 1
+    })
   }
 })
